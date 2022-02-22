3242f99b6f48b6a48af0a27d1dfe517b26cd9bfd
tony-playwright <99926773+tony-playwright@users.noreply.github.com>
2022-02-22 17:14:41 -0500

    Create SpringBootPlaywrightPdfApplication.java

---
+++ src/main/java/spring/boot/playwright/pdf/SpringBootPlaywrightPdfApplication.java
@@ -0,0 +1,13 @@
+package spring.boot.playwright.pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPlaywrightPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPlaywrightPdfApplication.class, args);
+	}
+
+}
